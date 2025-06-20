3ecbc6f15393ff17fc7c7e1112e0a908b528add1
Luis Álvarez <luigi>
2025-06-20 00:58:57 -0400

    edicion del searchbar

--- src/views/supervisor/inventory_view.py
+++ src/views/supervisor/inventory_view.py
@@ -1,9 +1,30 @@
 import flet as ft 
+from src.consts.colors import blue_color, purple_color
 
 def inventory(page: ft.Page):
+    
+    search_bar = ft.Row(
+        [
+           ft.TextField(hint_text="buscar por nombre...",
+                        prefix_icon=ft.Icons.SEARCH,
+                        width=300,
+                        border_radius= ft.border_radius.all(30),
+                        border_color=ft.Colors.GREY_300),
+            ft.IconButton(
+                bgcolor= ft.Colors.BLUE,
+                icon_color=ft.Colors.WHITE,
+                icon= ft.Icons.FILTER_LIST,
+                on_click= lambda e: print("despliega ventana emergente o menu para filtrar...")
+            ),
+        ],
+        alignment= ft.MainAxisAlignment.CENTER,
+        vertical_alignment= ft.CrossAxisAlignment.CENTER,
+        spacing= 10
+    )
     return ft.Column(
         [
-            ft.Text("Aqui va el inventario", size=50)
+           search_bar,
+           ft.Divider(height= 20, color= ft.Colors.GREY_300)
         ],
         horizontal_alignment= ft.CrossAxisAlignment.CENTER,
         alignment= ft.MainAxisAlignment.START,
